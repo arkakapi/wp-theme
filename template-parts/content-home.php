f9e7f88ddf8ca329aed28ac6fccb28185ffedb13
omer citak <mail@omercitak.com>
2020-04-03 03:53:27 +0300

    formating

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -7,10 +7,10 @@
             <div class="home-text">
 
                 <h1 class="hs-line-12 no-transp font-alt mb-50 mb-xs-30">
-					<?php the_field( 'main_title' ); ?>
+                    <?php the_field('main_title'); ?>
                 </h1>
                 <h2 class="hs-line-8 font-alt mb-50 mb-xs-30">
-					<?php the_field( 'main_slogan' ); ?>
+                    <?php the_field('main_slogan'); ?>
                 </h2>
 
             </div>
@@ -32,7 +32,7 @@
     <div class="container relative">
 
         <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
-			<?php the_field( 'about_title' ); ?>
+            <?php the_field('about_title'); ?>
         </h2>
 
         <div class="section-text mb-50 mb-sm-20">
@@ -41,16 +41,16 @@
                 <div class="col-md-4">
                     <blockquote>
                         <p>
-							<?php the_field( 'about_blockquote' ); ?>
+                            <?php the_field('about_blockquote'); ?>
                         </p>
                         <footer>
-							<?php the_field( 'about_blockquote_author' ); ?>
+                            <?php the_field('about_blockquote_author'); ?>
                         </footer>
                     </blockquote>
                 </div>
 
                 <div class="col-md-8 col-sm-12 mb-sm-100 mb-xs-60">
-					<?php the_field( 'about_content' ); ?>
+                    <?php the_field('about_content'); ?>
                 </div>
             </div>
         </div>
@@ -63,7 +63,7 @@
     <div class="container relative">
 
         <h2 class="section-title font-alt align-left mb-70 mb-sm-40">
-			<?php the_field( 'issues_title' ); ?>
+            <?php the_field('issues_title'); ?>
         </h2>
 
         <div class="section-text mb-50 mb-sm-20">
@@ -71,24 +71,24 @@
 
 
                 <ul class="nav nav-tabs tpl-tabs animate issues-tab">
-					<?php
-					$issues = get_field( 'issues' );
-					foreach ( $issues as $key => $val ) {
-						$key ++;
-						$class = $key == count( $issues ) ? "active" : "";
-						echo '<li class="' . $class . '">
+                    <?php
+                    $issues = get_field('issues');
+                    foreach ($issues as $key => $val) {
+                        $key++;
+                        $class = $key == count($issues) ? "active" : "";
+                        echo '<li class="' . $class . '">
                                       <a href="#issue' . $key . '" data-toggle="tab">' . $val['title'] . '</a>
                                   </li>';
-					}
-					?>
+                    }
+                    ?>
                 </ul>
 
                 <div class="tab-content tpl-tabs-cont section-text">
-					<?php
-					foreach ( $issues as $key => $val ) {
-						$key ++;
-						$class = $key == count( $issues ) ? "in active" : "";
-						?>
+                    <?php
+                    foreach ($issues as $key => $val) {
+                        $key++;
+                        $class = $key == count($issues) ? "in active" : "";
+                    ?>
                         <div class="tab-pane fade <?php echo $class; ?>" id="issue<?php echo $key; ?>">
                             <div class="row">
                                 <div class="col-md-4">
@@ -99,18 +99,18 @@
                                         <div class="col-sm-12">
                                             <ul class="nav nav-tabs tpl-tabs animate">
                                                 <li class="active">
-                                                    <a href="#issue<?php echo $key; ?>content" data-toggle="tab"><?php echo __( 'CONTENT', 'arkakapi' ); ?></a>
+                                                    <a href="#issue<?php echo $key; ?>content" data-toggle="tab"><?php echo __('CONTENT', 'arkakapi'); ?></a>
                                                 </li>
                                                 <li>
-                                                    <a href="#issue<?php echo $key; ?>buy" data-toggle="tab"><?php echo __( 'BUY', 'arkakapi' ); ?></a>
+                                                    <a href="#issue<?php echo $key; ?>buy" data-toggle="tab"><?php echo __('BUY', 'arkakapi'); ?></a>
                                                 </li>
                                             </ul>
                                             <div class="tab-content tpl-tabs-cont section-text">
                                                 <div class="tab-pane fade in active issue-content" id="issue<?php echo $key; ?>content">
-													<?php echo $val['content']; ?>
+                                                    <?php echo $val['content']; ?>
                                                 </div>
                                                 <div class="tab-pane fade" id="issue<?php echo $key; ?>buy">
-													<?php echo $val['buy_links']; ?>
+                                                    <?php echo $val['buy_links']; ?>
                                                 </div>
                                             </div>
                                         </div>
@@ -118,9 +118,9 @@
                                 </div>
                             </div>
                         </div>
-						<?php
-					}
-					?>
+                    <?php
+                    }
+                    ?>
                 </div>
 
 
@@ -131,14 +131,14 @@
 <!-- End About Section -->
 
 <!-- Divider -->
-<hr class="mt-0 mb-0 "/>
+<hr class="mt-0 mb-0 " />
 <!-- End Divider -->
 
 <!-- Subscription Section -->
 <section class="page-section" id="subscription">
     <div class="container relative">
         <h2 class="section-title font-alt mb-70 mb-sm-40">
-			<?php the_field( 'subscription_title' ); ?>
+            <?php the_field('subscription_title'); ?>
         </h2>
     </div>
 </section>
@@ -154,10 +154,10 @@
 
                 <div class="mt-140 mt-lg-80 mb-140 mb-lg-80">
                     <div class="banner-content">
-                        <h3 class="banner-heading font-alt"><?php the_field( 'subscription_subtitle' ); ?></h3>
-                        <div class="banner-decription"><?php the_field( 'subscription_content' ); ?></div>
+                        <h3 class="banner-heading font-alt"><?php the_field('subscription_subtitle'); ?></h3>
+                        <div class="banner-decription"><?php the_field('subscription_content'); ?></div>
                         <div class="local-scroll subscribe-buttons">
-							<?php the_field( 'subscription_buttons_html' ); ?>
+                            <?php the_field('subscription_buttons_html'); ?>
                         </div>
                     </div>
                 </div>
@@ -165,7 +165,7 @@
             </div>
 
             <div class="col-sm-6 banner-image wow fadeInUp">
-                <img src="<?php the_field( 'subscription_image' ); ?>" alt=""/>
+                <img src="<?php the_field('subscription_image'); ?>" alt="" />
             </div>
 
         </div>
@@ -175,7 +175,7 @@
 <!-- End Call Action Section -->
 
 <!-- Divider -->
-<hr class="mt-0 mb-0 "/>
+<hr class="mt-0 mb-0 " />
 <!-- End Divider -->
 
 <!-- Contact Section -->
@@ -183,7 +183,7 @@
     <div class="container relative">
 
         <h2 class="section-title font-alt mb-70 mb-sm-40">
-			<?php the_field( 'contact_title' ); ?>
+            <?php the_field('contact_title'); ?>
         </h2>
 
         <div class="row mb-60 mb-xs-40">
@@ -191,21 +191,21 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="row">
 
-					<?php foreach ( get_field( 'contact_tabs' ) as $key => $val ): ?>
+                    <?php foreach (get_field('contact_tabs') as $key => $val) : ?>
                         <div class="col-sm-4 col-lg-4 pt-20 pb-20 pb-xs-0">
                             <div class="contact-item">
                                 <div class="ci-icon">
                                     <i class="fa <?php echo $val['icon_class']; ?>"></i>
                                 </div>
                                 <div class="ci-title font-alt">
-									<?php echo $val['title']; ?>
+                                    <?php echo $val['title']; ?>
                                 </div>
                                 <div class="ci-text">
-									<?php echo $val['text']; ?>
+                                    <?php echo $val['text']; ?>
                                 </div>
                             </div>
                         </div>
-					<?php endforeach; ?>
+                    <?php endforeach; ?>
 
                 </div>
             </div>
@@ -215,7 +215,7 @@
         <!-- Contact Form -->
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-				<?php echo do_shortcode( '[contact-form-7 id="' . get_field( 'contact_form_id' ) . '" html_class="form contact-form"]' ); ?>
+                <?php echo do_shortcode('[contact-form-7 id="' . get_field('contact_form_id') . '" html_class="form contact-form"]'); ?>
             </div>
         </div>
         <!-- End Contact Form -->
@@ -225,10 +225,10 @@
 <!-- End Contact Section -->
 
 <?php
-$sponsors = get_field( 'sponsors' );
-if ( $sponsors ): ?>
+$sponsors = get_field('sponsors');
+if ($sponsors) : ?>
     <!-- Divider -->
-    <hr class="mt-0 mb-0 "/>
+    <hr class="mt-0 mb-0 " />
     <!-- End Divider -->
 
     <!-- Sponsors Section -->
@@ -236,18 +236,18 @@ if ( $sponsors ): ?>
         <div class="container relative">
 
             <h2 class="section-title font-alt mb-70 mb-sm-40">
-				<?php the_field( 'sponsors_title' ); ?>
+                <?php the_field('sponsors_title'); ?>
             </h2>
 
             <div class="row">
                 <div class="col-xs-12">
-					<?php foreach ( $sponsors as $sponsor ): ?>
-                        <div class="col-xs-6 col-md-<?php echo 12 / count( $sponsors ); ?> sponsor-item col-centered">
+                    <?php foreach ($sponsors as $sponsor) : ?>
+                        <div class="col-xs-6 col-md-<?php echo 12 / count($sponsors); ?> sponsor-item col-centered">
                             <a href="<?php echo $sponsor['description']; ?>" target="_blank">
-                                <img src="<?php echo $sponsor['sizes']['medium']; ?>" alt="<?php echo $sponsor['alt']; ?>"/>
+                                <img src="<?php echo $sponsor['sizes']['medium']; ?>" alt="<?php echo $sponsor['alt']; ?>" />
                             </a>
                         </div>
-					<?php endforeach; ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
 
